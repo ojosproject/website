b4c2b429106d5b9d2b5b0c0d8e9a67a9e26e7c54
Carlos Valdez <cvaldezh@uci.edu>
2023-10-30 16:09:16 -0700

    feat: added the story view w/ lorem text

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@
 
 import './App.css';
 import logo from './static/logo.png'
+import grandma from "./static/grandma.jpg"
 import Layout from './Layout'
 
 function App() {
@@ -13,6 +14,18 @@ function App() {
       <div className="photo_container">
         <img src={logo} alt="The logo of the Ojos Project" width="350" height="350"></img>
         <a href="https://markbaldw.in/url/" target="_blank" rel="noopener noreferrer">a uci research project</a>
+      </div>
+
+      <div className="story">
+        <div id="story_text">
+          <h1>Creating an easier way to stay connected with who you love.</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nam aliquam sem et tortor consequat id porta nibh. Diam ut venenatis tellus in metus vulputate eu scelerisque. Lorem ipsum dolor sit amet.</p>
+        </div>
+        <img id="story_image" src={grandma}></img>
+      </div>
+
+      <div className="our_team">
+        
       </div>
       
     </Layout>
