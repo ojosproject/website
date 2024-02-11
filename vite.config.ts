70b6ef330ed70268112dda1699cd5734f193a272
Carlos Valdez <cvaldezh@uci.edu>
2024-02-11 01:12:10 -0800

    fix(pages): temporary vite.config.ts fix
    `vite.config.ts` requires rollupOptions.input for every single
    page in the site. The point of having the site have prog. pages
    is to avoid having to do stuff like this. For now, these pages are
    automatically created with `.gitlab-ci.yml`. I don't think this
    is a good idea, but as long as it works...

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        news: resolve(__dirname, 'news/index.html'),
-      }
-    }
-  }
+  plugins: [react()]
 })
