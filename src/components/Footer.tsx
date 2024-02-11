ceb4f8eda429b836d96fb2fb8a36d120a9169d4a
Carlos Valdez <cvaldezh@uci.edu>
2024-02-10 19:01:12 -0800

    feat: added Header.tsx and Footer.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+// Footer.tsx
+// Ojos Project
+// 
+// This is the footer displayed everywhere on ojos.calejvaldez.com.
+
+function Footer() {
+    return (
+        <>
+
+        </>
+    )
+}
+
+export default Footer;
---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+// Header.tsx
+// Ojos Project
+// 
+// This is the header that is displayed everywhere on ojos.calejvaldez.com.
+
+function Header() {
+    return (
+        <>
+
+        </>
+    )
+}
+
+export default Header;
