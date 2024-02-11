47a229f7e669ac42809d4bae4d29a27c15ccca5d
Carlos Valdez <cvaldezh@uci.edu>
2024-02-10 20:06:34 -0800

    feat: recreated footer from original website

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,10 +4,12 @@
 // This is the footer displayed everywhere on ojos.calejvaldez.com.
 
 function Footer() {
+    // Temporary footer
+    // Once I want to add more information, I'll add an appropriate footer.
     return (
-        <>
-
-        </>
+        <footer>
+            <p className="footer_message">ojos means eyes, named after my grandma's nickname for me: <a href="https://translate.google.com/?sl=es&tl=en&text=mis+ojos" target="_blank" rel="noopener noreferrer">mis ojos</a></p>
+        </footer>
     )
 }
 
