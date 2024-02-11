79a2cf76e2e17373b9d3bd0ec13a376cc47de5c4
Carlos Valdez <cvaldezh@uci.edu>
2024-02-11 10:25:38 -0800

    fix: all html in /public/

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, "public/index.html"),
+        news: resolve(__dirname, "public/news/index.html"),
+        news_join_us: resolve(__dirname, "public/news/join-us/index.html"),
+        news_interviews: resolve(__dirname, "public/news/1706645600/index.html"),
+        broken_join_us: resolve(__dirname, "public/join-us/index.html")
+      }
+    }
+  }
 })
