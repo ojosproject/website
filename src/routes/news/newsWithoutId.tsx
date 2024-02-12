08bc7317fa0fa4b541ffadf3fb81f1e4d6e80444
Carlos Valdez <cvaldezh@uci.edu>
2024-02-12 14:35:29 -0800

    feat(news): added indicator for draft articles when date=0

--- src/routes/news/newsWithoutId.tsx
+++ src/routes/news/newsWithoutId.tsx
@@ -8,17 +8,21 @@ import "../../styles/newsWithId.css"
 import newsStand from '../../data/metadata.json'
 
 function ClickableNews(props: NewsArticle) {
+    let date_label = <blockquote>⚠️ THIS IS A DRAFT.<br/><br/>Information in this article may or may not be accurate and is subject to change.</blockquote>
+
+    if (props.date !== 0) {
+      // If the date is 0, it means it has not been published and is a draft.
+      date_label = <p>{new Date(props.date*1000).toLocaleDateString("PST", {
+        day: "2-digit",
+        month: "long",
+        year: "numeric"
+      })}</p>
+    }
     return <a href={"/news/"+props.id+"/"} className="news-stand-article">
         <img src={props.imageLocation} width="200"/>
         <div>
             <h3><span className="article-title">{props.title}</span> by {props.author}</h3>
-            <p>{
-                new Date(props.date*1000).toLocaleDateString("PST", {
-                    day: "numeric",
-                    month: 'short',
-                    year: 'numeric'
-                })
-                }</p>
+            {date_label}
             
             <p>{props.description}</p>
         </div>
