a729c87064d63de11963ee75aef7180c41d23901
Carlos Valdez <cvaldezh@uci.edu>
2024-02-12 23:35:33 -0800

    feat(root): specified SSH setup link to generation section

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -20,7 +20,7 @@ function Footer() {
                 <ul>
                     <li>Dev Team</li>
                     <li><a target="_blank" rel="noopener noreferrer" href="https://gitlab.com/ojosproject/">GitLab</a></li>
-                    <li><a target="_blank" rel="noopener noreferrer" href="https://docs.gitlab.com/ee/user/ssh.html">Setting up SSH</a></li>
+                    <li><a target="_blank" rel="noopener noreferrer" href="https://docs.gitlab.com/ee/user/ssh.html#generate-an-ssh-key-pair">Setting up SSH</a></li>
                     <li><a target="_blank" rel="noopener noreferrer" href="https://www.freecodecamp.org/news/how-to-write-commit-messages-maintainers-will-like/">Writing a commit</a></li>
                     <li><a target="_blank" rel="noopener noreferrer" href="https://www.geeksforgeeks.org/basic-linux-commands/">Basic Linux Commands</a></li>
                     <li><a target="_blank" rel="noopener noreferrer" href="https://quickref.me/bash">Bash Cheatsheet</a></li>
