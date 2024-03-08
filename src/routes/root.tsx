21432266fbf8c36ac298ae355b18e56bbae43731
Carlos Valdez <cvaldezh@uci.edu>
2024-03-08 01:11:04 -0800

    feat(root): added gravatar photos of team

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -7,14 +7,15 @@ import GrandmaImage from "/static/images/grandma.jpg"
 import "../styles/Root.css"
 
 export default function Root() {
-    function TeamMember(props: {name: string, position: string, organization: string, website?: string}) {
+    function TeamMember(props: {name: string, position: string, organization: string, gravatar_link: string, website?: string}) {
         return (
-            <div className='member_container'>
-                <a href={props.website} target="_blank" rel="noopener noreferrer">{props.name}</a>
+            <a className='member_container' target="_blank" rel="noopener noreferrer" href={props.website}>
+                <img src={props.gravatar_link+"?s=175&d=mp"} alt={`Gravatar for ${props.name}.`}/>
+                <br/>
+                <h3>{props.name}</h3>
                 <p>{props.organization}</p>
                 <p>{props.position}</p>
-                
-            </div>
+            </a>
         )
     }
     
@@ -32,12 +33,12 @@ export default function Root() {
             <div className='our_team' id="team">
                 <h1>Our Team</h1>
                 <div className='team_members'>
-                    <TeamMember name="Carlos Valdez" position='project lead' organization="university of california, irvine" website='https://calejvaldez.com/'/>
-                    <TeamMember name="Joseph Sweatt" position='engineering' organization="university of california, irvine" website='https://www.linkedin.com/in/josephsweattjr/'/>
-                    <TeamMember name="Ayush Jain" position='software developer' organization="university of california, irvine" website="https://ayushdotjain.com/"/>
-                    <TeamMember name="Isabella Gronich" position='researcher' organization="imperial valley college" website="mailto:igronich@students.imperial.edu"/>
-                    <TeamMember name="Jackie Sanchez"  position='researcher' organization="imperial valley college" website="mailto:jsanchez88@students.imperial.edu"/>
-                    <TeamMember name="Mark S. Baldwin, PhD" position='faculty advisor' organization="university of california, irvine" website='https://markbaldw.in/'/>
+                    <TeamMember name="Carlos Valdez" position='project lead' organization="university of california, irvine" gravatar_link='https://gravatar.com/avatar/41bb2938e02bf5326eb6b82ec02d919ca97cf68b376c4c5769fbba4acc85a190' website='https://calejvaldez.com/'/>
+                    <TeamMember name="Joseph Sweatt" position='engineering' organization="university of california, irvine" gravatar_link='https://gravatar.com/avatar/fac497a877e467035b306a201b938608120228662480b08e0477c59b4cfe347a' website='https://www.linkedin.com/in/josephsweattjr/'/>
+                    <TeamMember name="Ayush Jain" position='software developer' organization="university of california, irvine" gravatar_link='https://gravatar.com/avatar/e4c598f9826093429edec4c38d6bfb93968a59b512259b07e7d1ffe4ff7bb115' website="https://ayushdotjain.com/"/>
+                    <TeamMember name="Isabella Gronich" position='researcher' organization="imperial valley college" gravatar_link='https://gravatar.com/avatar/e4b66681a801324cb8b596783892b45e275d0c182789cb22a2af0c14ff73f0f8' website="mailto:igronich@students.imperial.edu"/>
+                    <TeamMember name="Jackie Sanchez"  position='researcher' organization="imperial valley college" gravatar_link='https://gravatar.com/avatar/60445455d0c9497dd7f2da0838c4b355d8686efab439067e79df497fbbbba3a3' website="mailto:jsanchez88@students.imperial.edu"/>
+                    <TeamMember name="Mark S. Baldwin, PhD" position='faculty advisor' organization="university of california, irvine" gravatar_link='https://gravatar.com/avatar/681e39c56221b204973cb6c3400ae235c6282f29862a742df02c2de30ca0df7c' website='https://markbaldw.in/'/>
                 </div>
             </div>
         </Layout>
