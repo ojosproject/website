048283e81a2ae64fd7cdcb7ad714f0ddfa74877d
Carlos Valdez <cvaldezh@uci.edu>
2024-03-19 10:11:36 -0700

    news(winter-2024-wrap): started draft

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,8 @@ export default defineConfig({
         news_interviews: resolve(__dirname, "public/news/1706645600/index.html"),
         news_urop_finances: resolve(__dirname, "public/news/urop-and-finances/index.html"),
         broken_join_us: resolve(__dirname, "public/join-us/index.html"),
-        news_home: resolve(__dirname, "public/news/welcome-home/index.html")
+        news_home: resolve(__dirname, "public/news/welcome-home/index.html"),
+        news_winter_2024_wrapped: resolve(__dirname, "public/news/winter-2024-wrap/index.html")
       }
     }
   }
