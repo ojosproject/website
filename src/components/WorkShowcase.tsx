ab8bf7b50962d14b24d946d64d3e4d26d2328e7d
Carlos Valdez <cvaldezh@uci.edu>
2024-10-13 20:21:41 -0700

    chore: nearly finalize new index

--- src/components/WorkShowcase.tsx
+++ src/components/WorkShowcase.tsx
@@ -1,21 +1,35 @@
 import "./WorkShowcase.scss";
 import Button from "./Button";
+import { ReactNode } from "react";
 
 export default function WorkShowcase(props: {
 	title: string;
-	description: string;
 	image: string;
 	image_alt: string;
 	button_label?: string;
 	button_link?: string;
+	button_secondary_label?: string;
+	button_secondary_link?: string;
+	children: ReactNode;
 }) {
 	return (
 		<div className="work-showcase">
 			<div className="work-showcase-left">
 				<h2>{props.title}</h2>
-				<p>{props.description}</p>
+				{props.children}
 				{props.button_label ? (
-					<Button label={props.button_label} href={props.button_link} />
+					<Button
+						type="PRIMARY"
+						label={props.button_label}
+						href={props.button_link}
+					/>
+				) : null}
+				{props.button_secondary_label ? (
+					<Button
+						type="SECONDARY"
+						label={props.button_secondary_label}
+						href={props.button_secondary_link}
+					/>
 				) : null}
 			</div>
 			<img src={props.image} alt={props.image_alt} />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,10 +16,9 @@ export default function Root() {
 			<header className="index-header">
 				<h1>Working with you for a better hospice experience.</h1>
 				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-					eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-					minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-					aliquip ex ea commodo consequat.
+					Ojos Project is a UCI-backed research endeavor in which we aim to
+					understand the needs of hospice patients, caregivers, and healthcare
+					specialists.
 				</p>
 			</header>
 
@@ -27,26 +26,49 @@ export default function Root() {
 				<h1>Our Work</h1>
 				<WorkShowcase
 					title="Iris: The Hospice Management System"
-					description="Iris is the Ojos Project's free and open-source hospice management system."
 					button_label="Read the Iris Docs"
 					button_link="/docs/iris/"
+					button_secondary_label="GitHub Repo"
+					button_secondary_link="https://github.com/ojosproject/iris/"
 					image={LogoHeader}
-					image_alt="Header of the Ojos Project"
-				/>
+					image_alt="Header of the Ojos Project">
+					<p>
+						Iris is the Ojos Project's hospice management system. Iris' goal is
+						to improve the hospice experience by providing various features,
+						such as <strong>medication logging</strong>,{" "}
+						<strong>video monitoring</strong>, and more.
+					</p>
+					<p>
+						Iris is being built using modern technologies, such as React/NextJS
+						and Rust/Tauri.
+					</p>
+
+					<p>
+						Iris is free and open-source software, meaning that you can install
+						it on any Windows/Mac/Linux device.
+					</p>
+				</WorkShowcase>
 				<WorkShowcase
 					title="Palliaview"
-					description="Palliaview is the Ojos Project's plug-and-play solution for Iris. It uses material specific to healthcare environments."
 					button_label="Read the Palliaview Docs"
 					button_link="/docs/palliaview/"
 					image={LogoHeader}
-					image_alt="Header of the Ojos Project"
-				/>
+					image_alt="Header of the Ojos Project">
+					<p>
+						Palliaview is the Ojos Project's plug-and-play solution for Iris. It
+						uses material specific to healthcare environments.
+					</p>
+				</WorkShowcase>
 				<WorkShowcase
 					title="Research"
-					description="Ojos Project is ultimately a research project. We're actively researching various technologies and interviewing healthcare professionals to get input for Iris and Palliaview."
 					image={LogoHeader}
-					image_alt="Header of the Ojos Project"
-				/>
+					image_alt="Header of the Ojos Project">
+					<p>
+						Ojos Project is ultimately a research project. We're actively
+						researching various technologies and interviewing healthcare
+						professionals to get input for Iris and Palliaview.
+					</p>
+				</WorkShowcase>
 			</div>
 
 			<div className="our_team" id="team">
