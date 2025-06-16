d02a43ec7ad30e15f0802ec3899b1fb75e950cf8
--global <self@calejvaldez.com>
2025-06-16 04:19:05 -0700

    add metatags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,10 +3,36 @@ import Header from "components/Header";
 import { ReactNode } from "react";
 import { Inter } from "next/font/google";
 import "./global.css";
+import { Metadata } from "next";
 
 const inter = Inter({
 	subsets: ["latin"],
 });
+const webpageTitle = "Ojos Project";
+const webpageDescription =
+	"Ojos Project is building Iris and Palliaview to improve the caregiver space with modern technology.";
+const webpageUrl = "https://ojosproject.org/";
+const webpageHeaderFile = "/images/header.png";
+
+export const metadata: Metadata = {
+	title: webpageTitle,
+	description: webpageDescription,
+	icons: ["/images/logo.png"],
+	openGraph: {
+		type: "website",
+		url: webpageUrl,
+		title: webpageTitle,
+		description: webpageDescription,
+		images: webpageHeaderFile,
+	},
+	twitter: {
+		card: "summary_large_image",
+		site: webpageUrl,
+		title: webpageTitle,
+		description: webpageDescription,
+		images: webpageHeaderFile,
+	},
+};
 
 export default function RootLayout({ children }: { children: ReactNode }) {
 	return (
