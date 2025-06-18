1d24cfa1747b1cf2f957be149ae93a8d4cd5edd3
Carlos Valdez <self@calejvaldez.com>
2025-06-17 19:31:12 -0700

    start footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,48 @@
+import Link from "next/link";
+import styles from "./Footer.module.css";
+import { ReactNode } from "react";
+
+type ListItemProps = {
+	href: string;
+	children: ReactNode;
+};
+
+function ListItem({ href, children }: ListItemProps) {
+	const isExternal = href.includes("https://");
+
+	return (
+		<li>
+			<Link
+				href={href}
+				target={isExternal ? "_blank" : null}
+				rel={isExternal ? "noopener noreferrer" : null}>
+				{children}
+			</Link>
+		</li>
+	);
+}
+
 export default function Footer() {
-	return <footer></footer>;
+	return (
+		<footer className={styles.footer}>
+			<ul>
+				<p>Ojos Project</p>
+				<ListItem href="https://github.com/ojosproject/">GitHub</ListItem>
+				<ListItem href="https://discord.gg/qZyQadbuMG">Discord</ListItem>
+			</ul>
+			<ul>
+				<p>Documentation</p>
+				<ListItem href="/docs/iris/">Iris</ListItem>
+				<ListItem href="/docs/palliaview/">Palliaview</ListItem>
+			</ul>
+			<div className={styles.grandmaMessageContainer}>
+				<p className={styles.grandmaMessage}>
+					ojos means eyes, named after my grandma's nickname for me:{" "}
+					<Link href="https://translate.google.com/?sl=es&tl=en&text=mis+ojos">
+						mis ojos
+					</Link>
+				</p>
+			</div>
+		</footer>
+	);
 }
