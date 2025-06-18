e97426ea260bbdad7887904183dc1ff5b50b2a02
Carlos Valdez <self@calejvaldez.com>
2025-06-17 19:41:08 -0700

    make a metadataWith() function
    Used to keep metadata the same on all pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,37 +3,18 @@ import Header from "components/Header";
 import { ReactNode } from "react";
 import { Inter } from "next/font/google";
 import "./global.css";
-import { Metadata } from "next";
+import { metadataWith } from "utils/metadata";
 
 const inter = Inter({
 	subsets: ["latin"],
 });
-const webpageTitle = "Ojos Project";
-const webpageDescription =
-	"Ojos Project is building Iris and Palliaview to improve the caregiver space with modern technology.";
-const webpageUrl = "https://ojosproject.org/";
-const webpageHeaderFile = "/images/header.png";
 
-export const metadata: Metadata = {
-	title: webpageTitle,
-	description: webpageDescription,
-	metadataBase: new URL(webpageUrl),
-	icons: ["/images/logo.png"],
-	openGraph: {
-		type: "website",
-		url: webpageUrl,
-		title: webpageTitle,
-		description: webpageDescription,
-		images: webpageHeaderFile,
-	},
-	twitter: {
-		card: "summary_large_image",
-		site: webpageUrl,
-		title: webpageTitle,
-		description: webpageDescription,
-		images: webpageHeaderFile,
-	},
-};
+export const metadata = metadataWith(
+	"Ojos Project — Iris, Palliaview, and caregiver research",
+	"Ojos Project is building Iris and Palliaview to improve the caregiver space with modern technology.",
+	"https://ojosproject.org/",
+	"/images/header.png",
+);
 
 export default function RootLayout({ children }: { children: ReactNode }) {
 	return (
---
+++ src/utils/metadata.ts
@@ -0,0 +1,29 @@
+import { Metadata } from "next";
+
+export function metadataWith(
+	webpageTitle: string,
+	webpageDescription: string,
+	webpageUrl: string,
+	webpageHeaderUrl: string,
+): Metadata {
+	return {
+		title: webpageTitle,
+		description: webpageDescription,
+		metadataBase: new URL("https://ojosproject.org/"),
+		icons: ["/images/logo.png"],
+		openGraph: {
+			type: "website",
+			url: webpageUrl,
+			title: webpageTitle,
+			description: webpageDescription,
+			images: webpageHeaderUrl,
+		},
+		twitter: {
+			card: "summary_large_image",
+			site: webpageUrl,
+			title: webpageTitle,
+			description: webpageDescription,
+			images: webpageHeaderUrl,
+		},
+	};
+}
