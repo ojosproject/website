fc5b5610da2730083b60de35cd598636716198b7
Carlos Valdez <self@calejvaldez.com>
2025-06-19 02:08:17 -0700

    add metadata to news

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -1,12 +1,20 @@
 import { articlesMetadata } from "utils/blog";
 import styles from "./page.module.css";
 import Link from "next/link";
+import { metadataWith } from "utils/metadata";
+
+export const metadata = metadataWith(
+	"Project News | Ojos Project",
+	"Want to learn about what's happening at Ojos Project? Start here at the Project News!",
+	"https://ojosproject.org/news/",
+	"/images/brand/oembed.png",
+);
 
 export default async function Page() {
 	return (
 		<section className={styles.blogContainer}>
 			<section className={styles.topmostContainer}>
-				<h1>Ojos Project Blog</h1>
+				<h1>Project News</h1>
 				<p>Stay up to date with project news, announcements, and more.</p>
 			</section>
 			<section>
