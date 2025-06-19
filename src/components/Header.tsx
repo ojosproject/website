38b21a49dd33eb61d6b52628ea865cbeefedee9d
Carlos Valdez <self@calejvaldez.com>
2025-06-19 02:04:15 -0700

    checkpoint

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { useRouter } from "next/navigation";
 import styles from "./Header.module.css";
 import Image from "next/image";
 import Link from "next/link";
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useState } from "react";
 
 type MenuItemProps = {
 	href: string;
@@ -20,6 +20,18 @@ function MenuItem({ href, children }: MenuItemProps) {
 
 export default function Header() {
 	const router = useRouter();
+	const [showMenu, setShowMenu] = useState(true);
+	const [hamburgerIcon, setHamburgerIcon] = useState(false);
+
+	useEffect(() => {
+		setHamburgerIcon(window.outerWidth < 480);
+		setShowMenu(window.outerWidth > 480);
+
+		return () => {
+			setHamburgerIcon(window.outerWidth < 480);
+			setShowMenu(window.outerWidth > 480);
+		};
+	}, []);
 
 	return (
 		<header className={styles.header}>
@@ -31,12 +43,25 @@ export default function Header() {
 				className={styles.ojosIcon}
 				onClick={() => router.push("/")}
 			/>
-			<ul className={styles.navigationMenu}>
-				<MenuItem href="/iris/">Iris</MenuItem>
-				<MenuItem href="/palliaview/">Palliaview</MenuItem>
-				<MenuItem href="/news/">News</MenuItem>
-				<MenuItem href="/team/">Our Team</MenuItem>
-			</ul>
+
+			{hamburgerIcon && (
+				<button
+					onClick={() => {
+						console.log(showMenu);
+						setShowMenu(!showMenu);
+					}}>
+					M
+				</button>
+			)}
+
+			{showMenu && (
+				<ul className={styles.navigationMenu}>
+					<MenuItem href="/iris/">Iris</MenuItem>
+					<MenuItem href="/palliaview/">Palliaview</MenuItem>
+					<MenuItem href="/news/">News</MenuItem>
+					<MenuItem href="/team/">Our Team</MenuItem>
+				</ul>
+			)}
 		</header>
 	);
 }
