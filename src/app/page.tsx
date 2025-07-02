d5d6d834572755467f9c8ee4492e966274a7ccf2
Carlos Valdez <self@calejvaldez.com>
2025-07-01 17:02:24 -0700

    style root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
 import styles from "./page.module.css";
+import { articlesMetadata } from "utils/blog";
 
 export default function Page() {
 	return (
 		<>
-			<section className={styles.promoIris}>
+			<section id="iris" className={styles.promo}>
 				<div className={styles.promoText}>
 					<h1>Iris for Desktop</h1>
 					<p>
@@ -23,34 +24,64 @@ export default function Page() {
 				</div>
 				<img src="/images/iris/promo/hub.png" />
 			</section>
-			<section>
-				<h1>Palliaview</h1>
-				<p>The best way to interact with Iris.</p>
+			<section id="palliaview" className={styles.promo}>
+				<div className={styles.promoText}>
+					<h1>Palliaview</h1>
+					<p>
+						<strong>The best way to interact with Iris.</strong> Palliaview is a
+						hardware solution designed by students at the Undergraduate Research
+						Lab at UC Irvine.
+					</p>
+					<div>
+						<Link href="/palliaview">
+							<button className="primary">Build now</button>
+						</Link>
+						<Link href="/palliaview/">
+							<button className="secondary">Learn more</button>
+						</Link>
+					</div>
+				</div>
+				<img src="/images/palliaview/promo/model.jpg" />
 			</section>
-			<section>
+			<section id="news" className={styles.newsroom}>
 				<h2>News</h2>
-				<p>Read the recent news by Ojos Project.</p>
+				<p>Read the recent news from Ojos Project.</p>
+				<div className={styles.cardContainer}>
+					{articlesMetadata.slice(0, 4).map((m) => {
+						return (
+							<div key={m.id} className={styles.newsCard}>
+								<h4>{m.title}</h4>
+								<p>{m.description}</p>
+								<p>{m.timestamp}</p>
+							</div>
+						);
+					})}
+				</div>
 			</section>
-			<section>
-				<h2>Mentorship</h2>
-				<p>
-					We started with the Undergraduate Research Lab. As we move beyond
-					university, we're mentoring the future developers of healthcare
-					software.
-				</p>
-			</section>
-			<section>
-				<h2>Get Involved</h2>
-				<p>
-					If you'd like to join our mission in creating great healthcare tools,
-					consider contributing to our open-source software.
-				</p>
-				<ul>
-					<li>Join our Discord to stay updated</li>
-					<li>Start a contributing group</li>
-					<li>Donate</li>
-				</ul>
-			</section>
+			<div className={styles.mentorAndGetInvolved}>
+				<section id="mentorship" className={styles.mentorshipOrGetInvolved}>
+					<h2>Mentoring the next generation</h2>
+					<p>
+						Our projects don't just build software, they build futures.
+						Contributors gain in-demand skills like Git-based collaboration,
+						modern web development, hardware prototyping and teamwork. We tackle
+						real-world problems, learn code review etiquette, and get feedback
+						from experienced engineers.
+					</p>
+				</section>
+				<section id="get-involved" className={styles.mentorshipOrGetInvolved}>
+					<h2>Get Involved</h2>
+					<p>
+						If you'd like to join our mission in creating great healthcare
+						tools, consider contributing to our open-source software.
+					</p>
+					<ul>
+						<li>Join our Discord to stay updated</li>
+						<li>Start a contributing group</li>
+						<li>Donate</li>
+					</ul>
+				</section>
+			</div>
 		</>
 	);
 }
